60e707dfc7dbfb151e1f44adc0ccc98a81a98ff0
dolphingarlic <dolphingarlic@gmail.com>
2020-09-30 20:02:09 +0200

    Tried to use pyppeteer and pdfgen instead of pdfkit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import asyncio
 import base64
-from urllib.request import urlopen 
+from urllib.request import urlopen
 
-from flask import Flask, make_response, request, render_template
+from quart import Quart, make_response, request, render_template
 from flaskext.markdown import Markdown
-import pdfkit
+import pdfgen
 
-app = Flask(__name__)
+loop = asyncio.get_event_loop()
+app = Quart(__name__)
 Markdown(app, extensions=['fenced_code'])
 
 
@@ -15,8 +17,8 @@ def home():
 
 
 @app.route('/statement', methods=['POST'])
-def statement():
-    f = request.form
+async def statement():
+    f = await request.form
     name = f['name']
     shortname = f['shortname']
     contest = f['contest']
@@ -30,35 +32,33 @@ def statement():
     constraints = f['constraints']
     scoring = f['scoring']
 
-    logo_b64 = base64.b64encode(urlopen(logo_url).read()).decode('utf-8')
+    if logo_url:
+        logo_b64 = base64.b64encode(urlopen(logo_url).read()).decode("utf-8")
+        logo_b64 = f'data:image/png;base64,{logo_b64}'
+    else:
+        logo_b64 = ''
 
-    html = render_template('statement.html',
-                           name=name,
-                           shortname=shortname,
-                           contest=contest,
-                           order=order,
-                           language=language,
-                           logo=f'data:image/png;base64,{logo_b64}',
-                           accent_colour=accent_colour,
-                           description=description,
-                           input=inp,
-                           output=output,
-                           constraints=constraints,
-                           scoring=scoring
-                          )
-    
-    options = {  
-        'footer-center': 'Page [page] of [topage]',
-        'quiet': '',
-        'javascript-delay' : '5000',
-        'page-size': 'A4',
-    }
-    pdf = pdfkit.from_string(html, False, options=options)
-    response = make_response(pdf)
+    html = await render_template('statement.html',
+                                 name=name,
+                                 shortname=shortname,
+                                 contest=contest,
+                                 order=order,
+                                 language=language,
+                                 logo=logo_b64,
+                                 accent_colour=accent_colour,
+                                 description=description,
+                                 input=inp,
+                                 output=output,
+                                 constraints=constraints,
+                                 scoring=scoring
+                                 )
+
+    # return html
+    pdf = await pdfgen.from_string(html)
+    response = await make_response(pdf)
     response.headers['Content-Type'] = 'application/pdf'
     response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
     return response
-    # return html
 
 if __name__ == '__main__':
     app.run()
